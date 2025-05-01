0876e687134f3aa7c2efe068c388b64c085b0084
dstet <me@dstet.me>
2025-04-30 23:25:22 -0400

    Add guess-indent, and update some confs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,3 +3,16 @@ require("config.lazy")
 vim.api.nvim_create_user_command('RenameIdentifier', function()
 	vim.lsp.buf.rename()
 end, {})
+
+vim.g.clipboard = {
+  name = 'wl-clipboard',
+  copy = {
+    ['+'] = 'wl-copy --foreground --type text/plain',
+    ['*'] = 'wl-copy --foreground --type text/plain',
+  },
+  paste = {
+    ['+'] = 'wl-paste --no-newline',
+    ['*'] = 'wl-paste --no-newline',
+  },
+  cache_enabled = true,
+}
